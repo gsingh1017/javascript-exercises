b4639785d285bfcf4f329b870d4457ed39708d86
gsingh1017 <182170594+gsingh1017@users.noreply.github.com>
2024-10-22 14:46:49 -0700

    Complete 02_repeatString exercise
    Used console.log to help see what was being outputted.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    // reutrns error when number is negative
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    // initialize new empty string
+    let newString = "";
+
+    // add string to newString with no spaces between num times
+    for (let i = 0; i < num; i++){
+        newString += `${string}`;
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
