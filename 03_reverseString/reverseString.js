1fa5a6152f680585afb20b8e1ebf19fe33d7677c
gsingh1017 <182170594+gsingh1017@users.noreply.github.com>
2024-10-22 18:28:24 -0700

    Complete 03_reverseString exercise
    Had to google to find chatAt() function to help solve this problem.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) { // string parameter
+    
+    // initialize new string
+    let newString = "";
 
+    // for loop adding last string character to newString one by one until 0
+    for (let i = string.length; i >= 0; i--) {
+        newString += string.charAt(i);
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
