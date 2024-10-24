5a9d20845b6d9d765814d5af32d9049aec925175
gsingh1017 <182170594+gsingh1017@users.noreply.github.com>
2024-10-24 16:29:25 -0700

    Complete 04_removeFromArray exercise
    Had to look at solution. I originally thought about finding length of original array and comparing each element one-by-one from original array to the arguments, but this was completly wrong.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,32 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    
+    /*
+
+    Below is code I wrote before looking at solution:
+
+
+    console.log(array);
+    console.log(value);
+
+    // initialize new array
+    let newArray = [];
+
+    // run through array length one by one
+    for (let i = 0; i < array.length - 1; i++) {
+        
+        // add array at i to newArray if it doesn't equal value
+        if (array.at(i) => !array.includes(args)); {
+            newArray = array.toSpliced(i,1);
+        }
+    }
+    */
+
+    // run through each element in original array
+    // if element doesn't equal an element from argument(s), 
+    // returns boolean true and filters
+    return array.filter(element => !args.includes(element));
+
+
 };
 
 // Do not edit below this line
