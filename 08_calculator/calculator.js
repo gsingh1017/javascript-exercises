56185e6e2d676eb91a70e1e4b21379be46d06d68
gsingh1017 <182170594+gsingh1017@users.noreply.github.com>
2024-10-25 12:56:43 -0700

    Complete 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,60 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+	return numOne + numTwo;
 };
 
-const subtract = function() {
-	
+
+const subtract = function(numOne, numTwo) {
+	return numOne - numTwo;
 };
 
-const sum = function() {
-	
+
+const sum = function(nums) {
+  // initialize num
+  let num = 0;
+
+  // add each element in array
+  for (element of nums) {
+    num += element;
+  }
+
+  return num;
 };
 
-const multiply = function() {
 
+const multiply = function(nums) {
+  // initialize num
+  // cannot start at 0 since we are multiplying
+  let num = 1;
+
+  // multiply each element in array
+  for (element of nums){
+    num *= element;
+  }
+
+  return num;
 };
 
-const power = function() {
-	
+
+const power = function(intOne, intTwo) {
+	return Math.pow(intOne, intTwo);
 };
 
-const factorial = function() {
-	
+
+const factorial = function(int) {
+
+  // initialize num
+  // cannot start at 0 since we are multiplying
+	let num = 1;
+
+  // start at 0 and multiply by int - i until int - i = 1 
+  for (let i = 0; i < int; i++){
+    num *= int - i
+  }
+  
+  return num;
+  
 };
+
 
 // Do not edit below this line
 module.exports = {
