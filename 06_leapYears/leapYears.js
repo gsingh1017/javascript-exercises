233e4ad8b64412bc45428d85567f2211feb8d59c
gsingh1017 <182170594+gsingh1017@users.noreply.github.com>
2024-10-25 11:43:18 -0700

    Complete 06_leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    // true if year is divisible by 4 and 400
+    // we immediately check for divisibility of 400 instead of 100 
+    if ((year % 4 === 0) && (year % 400 === 0)) {
+        return true;
+    }
+
+    // true if year is divisible by 4 and not 100
+    if ((year % 4 === 0) && (year % 100 != 0)) {
+        return true;
+    }
+
+    // everything else returns false
+    return false;
 
 };
 
