851168851f99f1b8e0764df046450d5ded6b72b0
gsingh1017 <182170594+gsingh1017@users.noreply.github.com>
2024-10-25 11:21:42 -0700

    Complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(intOne, intTwo) {
+ 
+    // boolean value to determine if parameters are integers
+    if ((Number.isInteger(intOne) != true) || (Number.isInteger(intTwo) != true)) {
+        return "ERROR";
+    }
 
+    // returns error if parameters are negative numbers
+    if ((intOne < 0) || (intTwo < 0)) {
+        return "ERROR";
+    }
+
+    // initialize number
+    let sum = 0;
+
+    if (intOne < intTwo) {
+
+        // for loop if intOne is less than intTwo
+        for (let i = 0; i < intTwo; i++){
+            sum += intOne + i;
+        }
+    } else {
+
+        // for loop if intTwo is less than intOne
+        for (let i = 0; i < intOne; i++){
+            sum += intTwo + i;
+        }
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
