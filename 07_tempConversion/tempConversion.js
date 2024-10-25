8a3e70a251f551954aff29288838e7058fa35e46
gsingh1017 <182170594+gsingh1017@users.noreply.github.com>
2024-10-25 12:14:48 -0700

    Complete 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  // converts to Celsius from Fahrenheit
+  // multiply number by 10, round the number, then divide by 10 to round by one decimal
+  return Math.round(((temp - 32) * (5/9)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  // converts to Fahrenheit from Celsius
+  // multiply number by 10, round the number, then divide by 10 to round by one decimal
+  return Math.round(((temp * (9/5)) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
