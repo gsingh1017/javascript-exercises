92fc38d145164f164a23f0175a8d2a542b040b09
gsingh1017 <182170594+gsingh1017@users.noreply.github.com>
2024-10-27 16:29:02 -0700

    Complete 09_palindromes exercise
    Had to use google to get some help with regular expressions (regex).

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    
+    // initialize new reverse string
+    let reverseString = "";
+
+    // newString removes all characters except a-z, A-Z, 0-9
+    let newString = string.replace(/\W/g, "")
+
+    // console.log(newString.toLowerCase()); 
+
+    // reverse string
+    for (let i = newString.length - 1; i >= 0; i--)
+        reverseString += newString[i];
+
+    // compare strings in lowercase to avoid case sensativity
+    if (newString.toLowerCase() === reverseString.toLowerCase()) {
+        return true;
+    } else {
+        return false; 
+    }
 
 };
 
