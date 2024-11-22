c42d6af25c10702ca60a2208cca9faa223d434a2
gsingh1017 <182170594+gsingh1017@users.noreply.github.com>
2024-11-22 11:20:55 -0800

    Add working code
    findTheOldest function works and all test cases pass without any of the objects being edited. Not at the fastest runtime.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    
+    const currentYear = new Date().getFullYear();
+    
+    let year = 0;
+    let oldest = 0;
+    let storePerson = "";
+
+    for (let i = 0; i < obj.length; i++) {
+
+        if (obj[i].yearOfDeath === undefined) {
+            year = currentYear;
+        } else {
+            year = obj[i].yearOfDeath;
+        }
+        
+        if ((year - obj[i].yearOfBirth) > oldest) {
+            oldest = year - obj[i].yearOfBirth;
+            storePerson = obj[i];
+        }
+    }
+    
+    return storePerson; 
 
 };
 
